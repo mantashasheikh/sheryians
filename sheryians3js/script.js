bb734b4d18aa394ea3f3d8644610a230d3ba745d
mantashasheikh <mantashasheikh95844@gmail.com>
2025-10-30 14:26:47 +0530

    common events : key down

--- sheryians3js/script.js
+++ sheryians3js/script.js
@@ -1,4 +1,4 @@
-let h1 = document.getElementById("main");
+let h1 = document.querySelector("h1");
 window.addEventListener("keydown" , function(dets){
     if(dets.key === " "){
         h1.textContent = "spc"
