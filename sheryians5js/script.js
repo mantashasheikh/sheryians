b72ecd8cf0c8698cb3bac06f0841a9ec542d7197
mantashasheikh <mantashasheikh95844@gmail.com>
2025-12-16 09:20:39 +0530

    sheryians5js

--- sheryians5js/script.js
+++ sheryians5js/script.js
@@ -1,6 +1,36 @@
 let form = document.querySelector("form")
-let inputs = document.querySelector()
+let inputs = document.querySelector("input")
+let main = document.querySelector("#main")
 
 form.addEventListener("submit", function(dets){
     dets.preventDefault()
+
+    let card = document.createElement("div")
+    card.classList.add("card")
+
+
+    let profile = document.createElement("profile")
+    profile.classList.add("profile")
+
+    let img = document.createElement("img")
+    img.setAttribute("src" , "")
+
+    let h5 = document.createElement("h5")
+    h5.textContent = "horse"
+    let h3 = document.createElement("h3")
+    h3.textContent = "Runnning"
+    let p = document.createElement("p")
+    p.textContent = "horse is  a very good animal . he runs fat."
+
+
+    profile.appendChild(img)
+    card.appendChild(profile)
+    card.appendChild(h3)
+    card.appendChild(h5)
+    card.appendChild(p)
+
+
+    main.appendChild(card)
+
+    // card.appendChild(profile)
 })
