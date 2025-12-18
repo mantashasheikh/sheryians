98a143d0d0e2889fc6906f52a36b9f5f0433760c
mantashasheikh <mantashasheikh95844@gmail.com>
2025-12-18 20:50:07 +0530

    sheryians12js

--- sheryians11js/script.js
+++ sheryians11js/script.js
@@ -3,13 +3,5 @@ let span = document.querySelector("span")
 
 
 inp.addEventListener("input" , function(){
-    let left = 20 - inp.value.length
-    span.textContent = left
-
-    if(left < 0){
-        span.style.color = "red"
-    }
-    else{
-        span.style.color = "black"
-    }
+    span.textContent = inp.value.length
 })
