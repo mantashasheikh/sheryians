b3aa593a7bd536626442b99ef7afdd2a4cd26dbd
mantashasheikh <mantashasheikh95844@gmail.com>
2025-12-18 21:28:12 +0530

    sheryians12js

--- sheryians12js/script.js
+++ sheryians12js/script.js
@@ -1,9 +1,9 @@
-let nam = document.querySelector("#name")
+let nm = document.querySelector("#name")
 let form = document.querySelector("form")
 
 form.addEventListener("submit" , function(dets){
     dets.preventDefault()
-    if(nam.value.length <= 2){
+    if(nm.value.length <= 2){
         document.querySelector("#hide").style.display = "initial"
     }else{
         document.querySelector("#hide").style.display = "none"
