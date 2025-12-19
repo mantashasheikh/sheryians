5158bcaf0ff3f11b43eacea73795ce8af500dd2f
mantashasheikh <mantashasheikh95844@gmail.com>
2025-12-19 10:11:53 +0530

    sheryians13js

--- sheryians13js/script.js
+++ sheryians13js/script.js
@@ -4,19 +4,30 @@ let form = document.querySelector("form")
 
 form.addEventListener("submit" , function(dets){
     dets.preventDefault()
-    const emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    const passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const passwordRegex =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+     document.querySelector("#emailError").textContent = ""
+     document.querySelector("#passwordError").textContent = ""
 
     let emailAns = emailRegex.test(email.value)
     let passwordAns = passwordRegex.test(password.value)
 
+    isValid = true
+
     if(!emailAns){
         document.querySelector("#emailError").textContent = "Email is incorrect"
         document.querySelector(".error").style.display = "initial"
+        isValid = false
     }
     if(!passwordAns){
         document.querySelector("#passwordError").textContent = "Password is incorrect"
         document.querySelector(".error").style.display = "initial"
+         isValid = false
+    }
+
+    if(isValid){
+        document.querySelector("#resultMessage").textContent = "everything is correct"
     }
 
 
