4e34e986deb1ed353c69e57cc376278d702d56ba
mantashasheikh <mantashasheikh95844@gmail.com>
2025-12-19 21:53:36 +0530

    sheryians15js

--- sheryians15js/script.js
+++ sheryians15js/script.js
@@ -1,1 +1,11 @@
+let count = 0
+let progress = document.querySelector(".progress-bar")
+let progressPercent = document.querySelector("#progressPercent")
 
+setInterval(function(){
+    if(count <= 99){
+        count++
+        progress.style.width = `${count}%`
+        progressPercent.textContent = `${count}%`
+    }
+},60)
