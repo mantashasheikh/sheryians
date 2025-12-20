f510ffda2aa374c82155ebd2d498806817e3f394
mantashasheikh <mantashasheikh95844@gmail.com>
2025-12-20 08:50:35 +0530

    sheryians16js

--- sheryians16js/script.js
+++ sheryians16js/script.js
@@ -1,2 +1,16 @@
+function setDarkOrLight (){
+    if(window.matchMedia('(prefers-color-scheme : dark )').matches){
+       document.body.classList.add("dark")
+       document.body.classList.remove("light")
+    }else{
+       document.body.classList.add("light")
+       document.body.classList.remove("dark")
+
+}
+}
+setDarkOrLight()
 
 
+window.matchMedia('(prefers-color-scheme : dark )').addEventListener("change", function(){
+    setDarkOrLight()
+})
